9eeeae400c7f3b349d1292bc634c24d592f32f2e
Christopher Hench <chris.l.hench@gmail.com>
2017-09-28 12:14:35 -0700

    add assignment puller

---
+++ assignments/pull-all.py
@@ -0,0 +1,8 @@
+import os
+import pandas as pd
+
+repo_df = pd.read_csv('repos.csv')
+
+for i in range(len(repo_df)):
+    folder = repo_df["Last Name"][i] + "-" + repo_df["First Name"][i]
+    os.system("cd " + folder + " &&" + " git pull")
